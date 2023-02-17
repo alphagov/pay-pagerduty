ba75a8e7a4c58dddcce9080b55ad8d536a83dc34
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2023-02-17 12:49:24 +0000

    Remove commented out test

--- spec/pager_duty_spec.rb
+++ spec/pager_duty_spec.rb
@@ -44,24 +44,4 @@ RSpec.describe PagerDuty do
     expect(request_1).to have_been_requested
     expect(request_2).to have_been_requested
   end
-
-  # it "override payload" do
-  #   pd = PagerDuty.new(api_token: "dummy")
-  #   from = DateTime.parse('2017-08-30T09:30:00+01:00')
-  #   to = DateTime.parse('2017-08-30T17:30:00+01:00')
-  #   payload = pd.override_payload(from, to, "my id")
-  #   parsed = JSON.parse(payload)
-  #   expected = {
-  #     override: {
-  #       start: '2017-08-30T09:30:00+01:00',
-  #       end: '2017-08-30T17:30:00+01:00',
-  #       user: {
-  #         id: 'my id',
-  #         type: 'user_reference'
-  #       }
-  #     }
-  #   }.to_json
-
-  #   expect(JSON.parse(payload)).to eq(JSON.parse(expected))
-  # end
 end
